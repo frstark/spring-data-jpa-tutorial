f0aead2cb1089d1dd7eb7ebf6486b41e9f0a0a69
frstark <frstark@tsn.at>
2021-11-10 11:40:54 +0100

    Initial commit

---
+++ src/test/java/com/stark/spring/data/jpa/tutorial/SpringDataJpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stark.spring.data.jpa.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
